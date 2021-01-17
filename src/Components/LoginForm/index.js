39a95b86dcee8b2596537e73709c9780eb914670
Solnovicode <s.wijsmassee@novi-education.nl>
2021-01-17 16:50:03 +0100

    basic Login

--- src/Components/LoginForm/index.js
+++ src/Components/LoginForm/index.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "./LoginForm.css";
+import {useForm} from "react-hook-form";
 
-export default function Index(){
+export default function LoginForm(){
+    const {register,handleSubmit} =useForm();
 
 
-    return<form>
+    function onSubmit(data){
+        console.log(data);
+    }
+    return <form onSubmit={handleSubmit(onSubmit)}>
 
         <label htmlFor='userName'>Username</label>
-        <input name="userName" type="text"/>
+        <input name="userName" type="text" ref={register}/>
         <label htmlFor='passWord'>Password</label>
-        <input name="passWord" type="password"/>
+        <input name="passWord" type="password" ref={register}/>
         <input type="submit"/>
         </form>;
 }
