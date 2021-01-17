46c09b5de4e686a147ba96cec59ce237be70a49e
Solnovicode <s.wijsmassee@novi-education.nl>
2021-01-17 17:20:23 +0100

    basic Login

--- src/Components/LoginForm/index.js
+++ src/Components/LoginForm/index.js
@@ -1,20 +1,26 @@
 import React from "react";
 import "./LoginForm.css";
 import {useForm} from "react-hook-form";
+import { IoWarningOutline} from "react-icons/all";
 
 export default function LoginForm(){
-    const {register,handleSubmit} =useForm();
+    const {register,handleSubmit,errors} =useForm();
 
 
     function onSubmit(data){
         console.log(data);
     }
-    return <form onSubmit={handleSubmit(onSubmit)}>
+    return <form className="loginContainer" onSubmit={handleSubmit(onSubmit)}>
 
         <label htmlFor='userName'>Username</label>
-        <input name="userName" type="text" ref={register}/>
+        <input name="userName" type="text" ref={register({required : true})}/>
+        {errors.userName && errors.userName.type === "required" &&
+        (<p><IoWarningOutline/> This field is required!</p>)}
+
         <label htmlFor='passWord'>Password</label>
-        <input name="passWord" type="password" ref={register}/>
+        <input name="passWord" type="password" ref={register({required : true})}/>
+        {errors.passWord && errors.passWord.type === "required" &&
+        (<p><IoWarningOutline/> Password is required!</p>)}
         <input type="submit"/>
         </form>;
 }
