a1f7f2a5db256f4622e82dd5888c8b586286b6a4
Solnovicode <s.wijsmassee@novi-education.nl>
2021-01-20 19:51:56 +0100

    basic Login

--- src/Components/LoginForm/index.js
+++ src/Components/LoginForm/index.js
@@ -1,15 +1,21 @@
 import React from "react";
 import "./LoginForm.css";
 import {useForm} from "react-hook-form";
-import { IoWarningOutline} from "react-icons/all";
+import {IoWarningOutline} from "react-icons/all";
 import axios from "axios";
+import {AuthContext,useAuthState} from "../Context/AuthContext";
+import {Link, useHistory} from 'react-router-dom';
+import{useEffect, useContext} from 'react';
+
+
+
 
 export default function LoginForm(){
     const {register,handleSubmit,errors} =useForm();
 
     async function onSubmit (data) {
         console.log("Wat is er ingevuld", data);
-        try{
+        try {
             const response = await axios.post(`https://polar-lake-14365.herokuapp.com/api/auth/signin`,
                 {
                     username: data.username,
@@ -20,21 +26,36 @@ export default function LoginForm(){
         } catch (error){
         }
     }
+    // context-functies
+    const { login } = useContext(AuthContext);
+    const { isAuthenticated } = useAuthState();
 
-    // function onSubmit(data){
-    //     console.log(data);
-    // }
+
+    // react-router dingen
+    const history = useHistory();
+
+    // Deze functie wordt elke keer afgevuurd als isAuthenticated (uit context) veranderd
+    useEffect(() => {
+        // als hij de waarde true heeft, DAN sturen we de gebruiker door!
+        if (isAuthenticated === true) {
+            history.push('/profile');
+        }
+    }, [isAuthenticated]);
+
+
+
     return <form className="loginContainer" onSubmit={handleSubmit(onSubmit)}>
 
-        <label htmlFor='userName'>Username</label>
+        <label htmlFor='username'>Username</label>
         <input name="username" type="text" ref={register({required : true})}/>
         {errors.username && errors.username.type === "required" &&
         (<p><IoWarningOutline/> This field is required!</p>)}
 
-        <label htmlFor='passWord'>Password</label>
+        <label htmlFor='password'>Password</label>
         <input name="password" type="password" ref={register({required : true})}/>
         {errors.password && errors.password.type === "required" &&
         (<p><IoWarningOutline/> Password is required!</p>)}
         <input type="submit"/>
+        <p>Heb je nog geen account? <Link to="/Register">Registreer</Link> je dan eerst.</p>
         </form>;
 }
--- src/Components/RegisterForm/index.js
+++ src/Components/RegisterForm/index.js
@@ -2,20 +2,70 @@ import "./RegisterForm.css";
 import React from "react";
 import {useForm} from "react-hook-form";
 import {IoWarningOutline} from "react-icons/all";
-
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import {useState} from "react";
 
 
 export default function RegisterForm(){
     const {register,handleSubmit,errors} =useForm();
-function onSubmit(data){
-    console.log(data);
+// function onSubmit(data){
+//     console.log(data);
+
+
+    // state voor gebruikers-feedback
+
+    const [loading, toggleLoading] = useState(false);
+    const [error, setError] = useState('');
+
+    // state voor gebruikers-feedback
+    const [createUserSuccess, setCreateUserSuccess] = useState(false);
+
+    async function onSubmit(data) {
+        // Dit alleen omdat we controlled components gebruiken, React-hook-form hoeft dit niet
+        toggleLoading(true);
+        setError('');
+
+        try {
+            // 1. Gebruik de data uit het formulier om een gebruiker aan te maken (check documentatie!)
+            const response = await axios.post('https://polar-lake-14365.herokuapp.com/api/auth/signup', {
+                username: data.username,
+                email: data.email,
+                password: data.password,
+                role: ["user"],
+            });
+
+            // // 2. Kijk goed wat je terugkrijgt!
+            console.log(response.data);
+
+
+            if (response.status === 200) {
+                // 3. Als het is gelukt, willen we in DIT component opslaan dat het gelukt is
+                setCreateUserSuccess(true);
+            }
+        } catch(e) {
+            console.error(e);
+            if (e.message.includes('400')) {
+                setError('Er bestaat al een account met deze gebruikersnaam');
+            } else {
+                setError('Er is iets misgegaan bij het verzenden. Probeer het opnieuw');
+            }
+        }
+
 }
-return <form className="registerContainer" onSubmit={handleSubmit(onSubmit)}>
+return (
+    <>
+    {/*4. Als het gelukt is, willen we een berichtje laten zien in de HTML, zoals:*/}
+    {createUserSuccess === true && (
+        <h2 className="message-success">Registration successful! 🥳 Click <Link to="/Login">here</Link> to log in!</h2>
+    )}
+
+    <form className="registerContainer" onSubmit={handleSubmit(onSubmit)}>
 
 
-    <label htmlFor='userName'>Username</label>
-    <input name="userName" type="text" ref={register({required : true})}/>
-    {errors.userName && errors.userName.type === "required" &&
+    <label htmlFor='username'>Username</label>
+    <input name="username" type="text" ref={register({required : true})}/>
+    {errors.username && errors.username.type === "required" &&
     (<p><IoWarningOutline/> This field is required!</p>)}
 
     <label htmlFor='password'>Password</label>
@@ -28,7 +78,12 @@ return <form className="registerContainer" onSubmit={handleSubmit(onSubmit)}>
     {errors.email && errors.email.type === "required" &&
     (<p><IoWarningOutline/> Email is required!</p>)}
 
-    <input type="submit"/>
+    <input type="submit"    disabled={loading}  />
+        {/*Zorg dat de gebruiker niet nog een keer kan klikken terwijl we een request maken*/}
 
-</form>;
+        {error && <p>{error}</p>}
+</form>
+        <p>If you already have an account click on <Link to="/Login">login</Link> to be redirected to our Login Page.</p>
+        </>
+ );
 }
