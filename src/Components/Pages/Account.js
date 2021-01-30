4bf66c8b8673031a541c41f9b10e3c9b938bb9c5
Solnovicode <s.wijsmassee@novi-education.nl>
2021-01-30 22:56:37 +0100

    basic Login

--- src/Components/Pages/Account.js
+++ src/Components/Pages/Account.js
@@ -1,10 +1,39 @@
-
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { useAuthState } from '../Context/AuthContext';
 
 function Account() {
     const { user } = useAuthState();
+
+    // State voor de data-status
+    const [error, setError] = useState('');
+    const [protectedData, setProtectedData] = useState('');
+
+    useEffect(() => {
+        async function getProtectedData() {
+            setError('');
+            try {
+                // haal de token op uit de local storage
+                const token = localStorage.getItem('token');
+
+                // haal de protected data op met de token meegestuurd
+                const response = await axios.get('https://polar-lake-14365.herokuapp.com/api/test/user', {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`,
+                    }
+                });
+
+                // zet deze data in de state zodat we dit in het component kunnen laten zien
+                setProtectedData(response.data);
+            } catch(e) {
+                setError('Er is iets misgegaan bij het ophalen van de data')
+            }
+        }
+
+        getProtectedData();
+    }, []);
 
     return (
         <>
@@ -18,9 +47,10 @@ function Account() {
             )}
 
             <h2>Afgeschermde content voor ingelogde gebruikers</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias cum debitis dolor dolore fuga id molestias
-                qui quo unde?</p>
-            <p>Terug naar de <Link to="/">Homepagina</Link></p>
+            {protectedData && <p>{protectedData}</p>}
+            {error && <p className="message-error">{error}</p>}
+
+            <p>Terug naar de <Link to="/">Home</Link></p>
         </>
     );
 }
